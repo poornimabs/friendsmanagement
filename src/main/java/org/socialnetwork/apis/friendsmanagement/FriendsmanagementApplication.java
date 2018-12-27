5812477dcd222c9c7c7e13f0a42e8f3004a1f614
poornimabs <poornimabs.gs31@gmail.com>
2018-12-27 18:11:24 +0530

    Initial Project Structure

---
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/FriendsmanagementApplication.java
@@ -0,0 +1,14 @@
+package org.socialnetwork.apis.friendsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsmanagementApplication.class, args);
+	}
+
+}
+
