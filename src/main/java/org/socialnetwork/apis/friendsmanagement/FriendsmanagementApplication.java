58f9888638d55fce261b561b11a55235fb5ca35c
poornimabs <poornimabs.gs31@gmail.com>
2018-12-28 15:50:24 +0530

    Added Swagger Api Configuration

--- src/main/java/org/socialnetwork/apis/friendsmanagement/FriendsmanagementApplication.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/FriendsmanagementApplication.java
@@ -2,8 +2,20 @@ package org.socialnetwork.apis.friendsmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+/**
+* Spring Boot Main Class
+*
+* @author  Poornima.BS
+* @version 1.0
+* @since   2018-12-28 
+*/
 
 @SpringBootApplication
+@Import({
+	SwaggerConfig.class
+})
 public class FriendsmanagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package org.socialnetwork.apis.friendsmanagement;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+* Swagger API Configuration
+*
+* @author  Poornima.BS
+* @version 1.0
+* @since   2018-12-28 
+*/
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	/**
+	* @return Docket
+	*/
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+	
+	/**
+	* @return ApiInfo
+	*/
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "Friends Management", 
+	      "API that does simple \"Friends Management\"", 
+	      "API TOS", 
+	      "Terms of service", 
+	      new Contact("Poornima BS", "https://github.com/PoornimaBS" ,"poornimabs.gs31@gmail.com"), 
+	      "License of API", "API license URL", Collections.emptyList());
+	}
+
+}
