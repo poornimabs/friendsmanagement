7b8085d5caf28047d82ccb962fa8c2cd970a7004
poornimabs <poornimabs.gs31@gmail.com>
2018-12-30 16:18:30 +0530

    Added ApplicationConstants

---
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/constant/ApplicationConstants.java
@@ -0,0 +1,18 @@
+package org.socialnetwork.apis.friendsmanagement.constant;
+
+/**
+* General constants used across application
+*
+* @author  Poornima.BS
+* @version 1.0
+* @since   1.0 
+*/
+
+public class ApplicationConstants {
+	
+	/**
+	 * The resource name for friendsmanagement.
+	 */
+	public static final String RESOURCE_PATH = "/friendsmanagement";
+
+}
