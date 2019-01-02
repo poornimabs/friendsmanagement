d149a09fdcbb693583dd1a2874ba29f33cb80da8
poornimabs <poornimabs.gs31@gmail.com>
2019-01-02 14:47:04 +0530

    Added GenericExceptionResponse

---
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/dto/GenericExceptionResponse.java
@@ -0,0 +1,45 @@
+package org.socialnetwork.apis.friendsmanagement.dto;
+
+import java.util.Date;
+
+/**
+ * ValidationResponse Data Transfer Object
+ *
+ * @author  Poornima.BS
+ * @version 1.0
+ * @since   1.0 
+ */
+
+public class GenericExceptionResponse {
+
+	private Date timestamp;
+	private String message;
+	private String details;
+
+	public GenericExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+}
