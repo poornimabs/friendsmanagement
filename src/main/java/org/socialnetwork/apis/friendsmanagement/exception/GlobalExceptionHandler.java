bfeeeeb883a61eaeb261880c2539f0ed4329ab16
poornimabs <poornimabs.gs31@gmail.com>
2019-01-02 14:52:28 +0530

    Added ExceptionHandler Methods

--- src/main/java/org/socialnetwork/apis/friendsmanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/exception/GlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package org.socialnetwork.apis.friendsmanagement.exception;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.socialnetwork.apis.friendsmanagement.constant.ApplicationExceptionConstants;
+import org.socialnetwork.apis.friendsmanagement.dto.GenericExceptionResponseDTO;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Global Exception Handling Controller
@@ -22,7 +26,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ExceptionHandler(DataIntegrityViolationException.class)
@@ -36,25 +40,21 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(RecordNotFoundException.class)
-	public GenericExceptionResponse handleRecordNotFoundException(RecordNotFoundException exception,
-			HttpServletRequest request) {
-		return formatException(exception);
+	public final ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException exception,
+			WebRequest request) {
+		GenericExceptionResponseDTO exceptionResponse = new GenericExceptionResponseDTO(new Date(), 
+				exception.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(UserDataInvalidException.class)
-	public GenericExceptionResponse handleUserDataInavlidException(UserDataInvalidException exception, 
-			HttpServletRequest request) {
-		List<String> details = new ArrayList<String>();
-		details.add(exception.getLocalizedMessage());
-		GenericExceptionResponse error = new GenericExceptionResponse(exception.getMessage(), details);
-		return error;
-	}
-
-	private GenericExceptionResponse formatException(Exception exception) {
-		List<String> details = new ArrayList<String>();
-		details.add(exception.getLocalizedMessage());
-		GenericExceptionResponse error = new GenericExceptionResponse(exception.getMessage(), details);
-		return error;
-	}
-
+	public final ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		GenericExceptionResponseDTO exceptionResponse = new GenericExceptionResponseDTO(new Date(), 
+				ApplicationExceptionConstants.VALIDATION_FAILED,
+				ex.getBindingResult().toString());
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	} 
+	
 }
