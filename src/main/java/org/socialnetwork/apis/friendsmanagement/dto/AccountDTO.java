941beb895de3654437fea8034b87920748701e7e
poornimabs <poornimabs.gs31@gmail.com>
2019-01-02 14:55:57 +0530

    Added Hibernate Validators for Fields

--- src/main/java/org/socialnetwork/apis/friendsmanagement/dto/AccountDTO.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/dto/AccountDTO.java
@@ -1,5 +1,11 @@
 package org.socialnetwork.apis.friendsmanagement.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 /**
 * User Account Data Transfer Object
 *
@@ -9,10 +15,19 @@ package org.socialnetwork.apis.friendsmanagement.dto;
 */
 public class AccountDTO {
 	
+	@NotEmpty(message="Email cannot be an empty string")
+	@NotNull(message="Email cannot be null")
+	@Email(message="Please provide a valid email address")
 	private String email;
 	
+	@NotEmpty(message="Username cannot be an empty string")
+	@NotNull(message="Username cannot be null")
+	@Size(min=2, message="Name should have atleast 2 characters")
 	private String username;
 	
+	@NotEmpty(message="Password cannot be an empty string")
+	@NotNull(message="Password cannot be null")
+	@Size(min=7, message="Password should have atleast 2 characters")
 	private String password;
 	
 	public String getEmail() {
