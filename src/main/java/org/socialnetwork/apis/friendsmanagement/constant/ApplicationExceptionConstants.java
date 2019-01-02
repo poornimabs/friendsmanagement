be0d6bbae072f8887a9c967306da3d68a4bdd5bd
poornimabs <poornimabs.gs31@gmail.com>
2019-01-03 01:42:15 +0530

    Updated Application Exception Constants

--- src/main/java/org/socialnetwork/apis/friendsmanagement/constant/ApplicationExceptionConstants.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/constant/ApplicationExceptionConstants.java
@@ -22,5 +22,41 @@ public class ApplicationExceptionConstants {
 	/**
 	 * User Request Data Validation Failed
 	 */
-    public static final String VALIDATION_FAILED = "User Validation Failed - Invalid Data For Account Creation";
+    public static final String USER_VALIDATION_FAILED = "Validation Failed - Invalid Data For User Account Creation";
+    
+    /**
+	 * Friend Connection Data Validation
+	 */
+    public static final String FRIENDRELATION_VALIDATION_FAILED = "Validation Failed - Invalid Data For Friend Connection";
+    
+    /**
+	 * Friend Connection Data Validation - Friend Connection Requires
+	 *  Two Users - Greater or Less than will give an Error
+	 */
+    public static final String FRIENDS_CONNECTION = "Friend Connection Requires Two Users";
+    
+    /**
+	 *Validates Email Address
+	 */
+    public static final String INVALID_EMAIL = "Please provide a valid email address";
+    
+    /**
+     * Friend Relation Already Exists
+     */
+    public static final String FRIENDSHIP_EXISTS = "Friendship already exists";
+    
+    /**
+     * Friend Relation Already Exists and Blocked
+     */
+    public static final String FRIENDSHIP_BLOCKED = "Friend connection exists and status is blocked";
+    
+    /**
+     * User Account Not Exists
+     */
+    public static final String USER_ACCOUNT_NOT_EXISTS = "Please register the user";
+    
+    /**
+     * Friend Relation Already Exists and Blocked
+     */
+    public static final String UNSUBSCRIBED_FRIENDSHIP_BLOCKED = "Friend connection request is blocked by the user";
 }
