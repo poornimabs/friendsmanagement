6c39c85c5072024a6ba3613b376c9be6593066b0
poornimabs <poornimabs.gs31@gmail.com>
2019-01-02 14:43:25 +0530

    Renamed ExceptionConstants File and added new constants

--- src/main/java/org/socialnetwork/apis/friendsmanagement/constant/ApplicationExceptionConstants.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/constant/ApplicationExceptionConstants.java
@@ -7,11 +7,20 @@ package org.socialnetwork.apis.friendsmanagement.constant;
  * @version 1.0
  * @since   1.0 
  */
-public class ExceptionConstants {
-
-	public static final String INAVLID_DATA = "Invalid Data For Account Creation";
-
+public class ApplicationExceptionConstants {
+	
+	/**
+	 * Record Not Found Exception
+	 */
 	public static final String RECORD_NOT_FOUND = "Record Not Found";
-
+	
+	/**
+	 * No Common Friends Exception
+	 */
 	public static final String NO_COMMON_FRIENDS = "No Common Friends";
+	
+	/**
+	 * User Request Data Validation Failed
+	 */
+    public static final String VALIDATION_FAILED = "User Validation Failed - Invalid Data For Account Creation";
 }
