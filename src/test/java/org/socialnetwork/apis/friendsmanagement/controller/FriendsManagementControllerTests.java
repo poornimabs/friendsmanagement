69cc18227e7f010ce389625109680e6f7f85bc86
poornimabs <poornimabs.gs31@gmail.com>
2019-01-04 05:27:49 +0530

    Added Test Case

---
+++ src/test/java/org/socialnetwork/apis/friendsmanagement/controller/FriendsManagementControllerTests.java
@@ -0,0 +1,67 @@
+package org.socialnetwork.apis.friendsmanagement.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.socialnetwork.apis.friendsmanagement.dto.AccountDTO;
+import org.socialnetwork.apis.friendsmanagement.entity.UserEntity;
+import org.socialnetwork.apis.friendsmanagement.service.FriendConnectionService;
+import org.socialnetwork.apis.friendsmanagement.service.NotificationService;
+import org.socialnetwork.apis.friendsmanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = FriendsManagementController.class)
+public class FriendsManagementControllerTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private UserService userService;
+	
+	@MockBean
+	private FriendConnectionService friendConnectionService;
+	
+	@MockBean
+	private NotificationService notificationService;
+	
+	@Test
+	public void account() throws Exception {
+		UserEntity response = new UserEntity("test@gmail.com","test","test@123");
+		AccountDTO accountDTO = new AccountDTO();
+		accountDTO.setUsername("test");
+		accountDTO.setEmail("test@gmail.com");
+		accountDTO.setPassword("test@123");
+		Mockito.when(
+				userService.account(accountDTO)).thenReturn(response);
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+	    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+	    String requestJson=ow.writeValueAsString(accountDTO );
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
+				"/friendsmanagement/account").accept(
+				MediaType.APPLICATION_JSON).content(requestJson)
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		String expected = "{success:true}";
+
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(),false);
+	}
+
+}
