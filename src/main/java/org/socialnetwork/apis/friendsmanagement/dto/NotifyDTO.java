c44ceebfd7aa004c23a5483e09cc631f5c7e44dd
poornimabs <poornimabs.gs31@gmail.com>
2019-01-03 11:31:53 +0530

    Added Resource Vaalidations

--- src/main/java/org/socialnetwork/apis/friendsmanagement/dto/NotifyDTO.java
+++ src/main/java/org/socialnetwork/apis/friendsmanagement/dto/NotifyDTO.java
@@ -2,6 +2,11 @@ package org.socialnetwork.apis.friendsmanagement.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * NotifyDTO Data Transfer Object
  *
@@ -13,7 +18,14 @@ import java.io.Serializable;
 public class NotifyDTO implements Serializable{
 	private static final long serialVersionUID = -1822135377222820305L;
 
+	@NotEmpty(message="Sender email cannot be an empty string")
+	@NotNull(message="Sender email cannot be null")
+	@Email(message="Please provide a valid email address")
 	private String sender;
+	
+	@NotEmpty(message="Notification cannot be an empty string")
+	@NotNull(message="Notification cannot be null")
+	@Size(min=2, message="Notification should have atleast 2 characters")
 	private String text;
 
 	public String getSender() {
