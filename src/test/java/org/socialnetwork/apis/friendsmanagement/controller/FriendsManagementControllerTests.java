98df34aeeb701185acbc5467a2c0db2426f11185
poornimabs <poornimabs.gs31@gmail.com>
2019-01-07 01:42:23 +0530

    Added Unit Test Cases

--- src/test/java/org/socialnetwork/apis/friendsmanagement/controller/FriendsManagementControllerTests.java
+++ src/test/java/org/socialnetwork/apis/friendsmanagement/controller/FriendsManagementControllerTests.java
@@ -1,5 +1,6 @@
 package org.socialnetwork.apis.friendsmanagement.controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
@@ -10,6 +11,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.socialnetwork.apis.friendsmanagement.dto.AccountDTO;
+import org.socialnetwork.apis.friendsmanagement.dto.FriendConnectionDTO;
+import org.socialnetwork.apis.friendsmanagement.dto.FriendsListDTO;
+import org.socialnetwork.apis.friendsmanagement.dto.NotifyDTO;
 import org.socialnetwork.apis.friendsmanagement.dto.UserEmailDTO;
 import org.socialnetwork.apis.friendsmanagement.entity.UserEntity;
 import org.socialnetwork.apis.friendsmanagement.service.FriendConnectionService;
@@ -19,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -83,15 +88,74 @@ public class FriendsManagementControllerTests {
 	    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
 	    String requestJson = ow.writeValueAsString(userEmail);
 	    Mockito.when(
-				friendConnectionService.friendsList(userEmail)).thenReturn((friendList));
+				friendConnectionService.friendsList(Mockito.any())).thenReturn((friendList));
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
 				"/friendsmanagement/friends").accept(
 				MediaType.APPLICATION_JSON).content(requestJson)
 				.contentType(MediaType.APPLICATION_JSON);
 
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
+		MockHttpServletResponse response = result.getResponse();
+		FriendsListDTO fiendListDTO = new FriendsListDTO(friendList);
+		String responsejson = ow.writeValueAsString(fiendListDTO);
+		JSONAssert.assertEquals(responsejson, response.getContentAsString(),false);
+		assertEquals(200, response.getStatus());
 		assertNotNull(result);
+	}
+	
+	@Test
+	public void commonFriends() throws Exception  {
+		List<String> friendList= new ArrayList<String>();
+		friendList.add("test3@example.com");
+		friendList.add("test4@example.com");
+		FriendConnectionDTO friendConnDTO = new FriendConnectionDTO();
+		friendConnDTO.setFriends(friendList);
+		
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+	    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+	    String requestJson = ow.writeValueAsString(friendConnDTO);
+	    Mockito.when(
+				friendConnectionService.commonFriends(Mockito.any())).thenReturn((friendList));
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
+				"/friendsmanagement/common").accept(
+				MediaType.APPLICATION_JSON).content(requestJson)
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		FriendsListDTO fiendListDTO = new FriendsListDTO(friendList);
+		String responsejson = ow.writeValueAsString(fiendListDTO);
+		JSONAssert.assertEquals(responsejson, response.getContentAsString(),false);
+		assertEquals(200, response.getStatus());
+		assertNotNull(result);
+		
+	}
+	
+	@Test
+	public void notification() throws Exception  {
+		NotifyDTO notifyDTO = new NotifyDTO();
+		notifyDTO.setSender("test1.example.com");
+		notifyDTO.setText("Hello");
+		List<String> friendList= new ArrayList<String>();
+		friendList.add("test3@example.com");
+		friendList.add("test4@example.com");
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+	    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+	    String requestJson = ow.writeValueAsString(notifyDTO);
+	    Mockito.when(
+	    		notificationService.notify(Mockito.any())).thenReturn((friendList));
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
+				"/friendsmanagement/notify").accept(
+				MediaType.APPLICATION_JSON).content(requestJson)
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		assertEquals(400, response.getStatus());
+		assertNotNull(result);
+		
 	}
 
 }
